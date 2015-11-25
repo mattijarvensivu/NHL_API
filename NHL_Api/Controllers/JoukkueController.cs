4dfa58d16fd455325d8394ede5cc70e4efa2772b
mattijarvensivu <mattijarvensivu93@gmail.com>
2015-11-25 21:49:38 +0200

    eka push

---
+++ NHL_Api/Controllers/JoukkueController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NHL_Api.Models;
+
+namespace NHL_Api.Controllers
+{
+    public class JoukkueController : ApiController
+    {
+        private H4102_3Entities db = new H4102_3Entities();
+
+        // GET: api/Joukkue
+        public IQueryable<Joukkue> GetJoukkues()
+        {
+            return db.Joukkue;
+        }
+
+        // GET: api/Joukkue/5
+        [ResponseType(typeof(Joukkue))]
+        public async Task<IHttpActionResult> GetJoukkue(int id)
+        {
+            Joukkue joukkue = await db.Joukkue.FindAsync(id);
+            if (joukkue == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(joukkue);
+        }
+
+        // PUT: api/Joukkue/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutJoukkue(int id, Joukkue joukkue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != joukkue.idJoukkue)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(joukkue).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JoukkueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Joukkue
+        [ResponseType(typeof(Joukkue))]
+        public async Task<IHttpActionResult> PostJoukkue(Joukkue joukkue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Joukkue.Add(joukkue);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = joukkue.idJoukkue }, joukkue);
+        }
+
+        // DELETE: api/Joukkue/5
+        [ResponseType(typeof(Joukkue))]
+        public async Task<IHttpActionResult> DeleteJoukkue(int id)
+        {
+            Joukkue joukkue = await db.Joukkue.FindAsync(id);
+            if (joukkue == null)
+            {
+                return NotFound();
+            }
+
+            db.Joukkue.Remove(joukkue);
+            await db.SaveChangesAsync();
+
+            return Ok(joukkue);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool JoukkueExists(int id)
+        {
+            return db.Joukkue.Count(e => e.idJoukkue == id) > 0;
+        }
+    }
+}
---
+++ NHL_Api/Controllers/PelaajaController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NHL_Api.Models;
+
+namespace NHL_Api.Controllers
+{
+    public class PelaajaController : ApiController
+    {
+        private H4102_3Entities db = new H4102_3Entities();
+
+        // GET: api/Pelaaja
+        public IQueryable<Pelaaja> GetPelaajas()
+        {
+            return db.Pelaaja;
+        }
+
+        // GET: api/Pelaaja/5
+        [ResponseType(typeof(Pelaaja))]
+        public async Task<IHttpActionResult> GetPelaaja(int id)
+        {
+            Pelaaja pelaaja = await db.Pelaaja.FindAsync(id);
+            if (pelaaja == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pelaaja);
+        }
+
+        // PUT: api/Pelaaja/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPelaaja(int id, Pelaaja pelaaja)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pelaaja.idPelaaja)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(pelaaja).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PelaajaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Pelaaja
+        [ResponseType(typeof(Pelaaja))]
+        public async Task<IHttpActionResult> PostPelaaja(Pelaaja pelaaja)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Pelaaja.Add(pelaaja);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = pelaaja.idPelaaja }, pelaaja);
+        }
+
+        // DELETE: api/Pelaaja/5
+        [ResponseType(typeof(Pelaaja))]
+        public async Task<IHttpActionResult> DeletePelaaja(int id)
+        {
+            Pelaaja pelaaja = await db.Pelaaja.FindAsync(id);
+            if (pelaaja == null)
+            {
+                return NotFound();
+            }
+
+            db.Pelaaja.Remove(pelaaja);
+            await db.SaveChangesAsync();
+
+            return Ok(pelaaja);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PelaajaExists(int id)
+        {
+            return db.Pelaaja.Count(e => e.idPelaaja == id) > 0;
+        }
+    }
+}
